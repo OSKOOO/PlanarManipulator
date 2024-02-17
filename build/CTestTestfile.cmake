140efb5ebd138151d0f6db0bb6cc4b0391a094ad
Omar Kolt <111469977+OSKOOO@users.noreply.github.com>
2024-02-17 00:24:06 -0800

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/obot/1X/submit/PlanarManipulator
-# Build directory: /home/obot/1X/submit/PlanarManipulator/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
